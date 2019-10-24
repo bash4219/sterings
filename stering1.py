6b249928d1ba6dbd8d92fc677f7b32e97c5018ff
bash4219 <kevin.bash18@gmail.com>
2019-10-24 15:28:02 -0500

    hi

--- stering1.py
+++ stering1.py
@@ -1,2 +1,22 @@
-duvisdvsusdv
+# Strings
 
+# Concatenation
+#    2 or more strings and put them together
+
+firstName = "hi"
+lastName = "haw are you doing"
+
+print(firstName + " " + lastName)
+
+# Repetition
+#   repetition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
