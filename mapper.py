816d477c952ca94c3623f7bd24ffcbd92f8b1ffe
bash4219 <kevin.bash18@gmail.com>
2019-10-28 15:46:52 -0500

    new stuf

--- mapper.py
+++ mapper.py
@@ -5,3 +5,17 @@ def letterToIndex(letter):
     if idx == -1:       #means that it wasn't in the alphabet
         print("error:", letter, "is not in the alphabet")
     return idx
+
+
+def indexToLetter(idx):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    letter = ''
+    if idx >= len(alphabet):
+        print("error:", idx, "is too large.")
+    elif idx < 0:
+        print("error:", idx, "is too small.")
+    else:
+        letter = alphabet[idx]
+    return letter
+
